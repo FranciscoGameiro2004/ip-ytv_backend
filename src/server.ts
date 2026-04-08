2ab5251e87f296a95981f4a663894f9304c2bca9
Francisco Gameiro <franciscogameiro@tutamail.com>
2026-04-08 12:00:07 +0100

    feat: :sparkles: Addition of a general router middleware to be expanded

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from "express"
 import type { Application } from "express"
 import cors from "cors"
 import { env } from "node:process"
+import { router } from "./routes/index.ts"
 
 const app: Application = express()
 
 app.use(cors())
+app.use('/', router)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
