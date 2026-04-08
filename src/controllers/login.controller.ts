9f80c0b2452665dfce012a01334fa3f6b97da2ba
Francisco Gameiro <franciscogameiro@tutamail.com>
2026-04-08 23:56:49 +0100

    fix: :bug: Fixed type issue at login middleware
    Fixes issue where the error value at the `jwt.sign` callback didn't had its correct typing

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -25,8 +25,8 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
     bcrypt.compare(data.password, user.hash, (err, result) => {
         if (result) {
-            jwt.sign({ username: user.username, role: user.role }, env.JWT_SECRET!, {}, (error: Error, token) => {
-                if (!error) {
+            jwt.sign({ username: user.username, role: user.role }, env.JWT_SECRET!, {}, (error: Error | null, token) => {
+                if (error !== null) {
                     res.status(200).json({ token: token })
                     return
                 } else {
