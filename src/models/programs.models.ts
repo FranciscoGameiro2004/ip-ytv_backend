eaf7e146954b67d879613f1f219cb343a300cc6a
Francisco Gameiro <franciscogameiro@tutamail.com>
2026-04-09 23:07:23 +0100

    feat: :sparkles: Introduction of a Program model
    Implementation of a `Program` schema and model through Mongoose

---
+++ src/models/programs.models.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema } from "mongoose";
+
+const programSchema = new Schema({
+    channelId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Channel',
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    flexibleTime: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    startTime: {
+        type: Schema.Types.Date,
+        required: true,
+    },
+    endTime: {
+        type: Schema.Types.Date,
+        required: function () {
+            return this.flexibleTime
+        },
+    },
+    maxVideos: {
+        type: Schema.Types.Int32,
+        min: 1,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['byYTChannel', 'byYTPlaylist'],
+    },
+    ytChannelId: {
+        type: String,
+        required: function () {
+            return this.type === 'byYTChannel'
+        },
+        enable: function () {
+            return this.type === 'byYTChannel'
+        },
+    },
+    ytPlaylistId: {
+        type: String,
+        required: function () {
+            return this.type === 'byYTPlaylist'
+        },
+        enable: function () {
+            return this.type === 'byYTPlaylist'
+        },
+    },
+    ytVideoSearchMode: {
+        type: String,
+        required: true,
+        enum: ['alwaysNewer', 'episode']
+    },
+    ytVideoInvertedOrder: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    ytLastVideoId: {
+        type: String,
+        required: false,
+    }
+})
+
+export const Program = mongoose.model('Program', programSchema)
