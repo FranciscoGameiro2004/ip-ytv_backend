f03e6a168f6130f66e0fc8ef58d7036f8ed64f75
Francisco Gameiro <franciscogameiro@tutamail.com>
2026-04-17 10:15:52 +0100

    feat: ✨ Addition of the route `live` to be expanded

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import express  from "express";
 import { userRouter } from "./users.routes.ts";
 import { loginRouter } from "./login.routes.ts";
 import { channelRouter } from "./channels.routes.ts";
+import { liveRouter } from "./live.routes.ts";
 
 export const router = express.Router()
 
 router.use('/users', userRouter)
 router.use('/login', loginRouter)
 router.use('/channels', channelRouter)
+router.use('/live', liveRouter)
